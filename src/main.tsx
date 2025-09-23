f97e1752792448b95a35e4ffac45d73cc66b1225
muritador5050 <muritador5050@gmail.com>
2025-09-23 20:50:54 +0100

    i implement PWA

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import './index.css';
 
+import 'virtual:pwa-register';
+
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
