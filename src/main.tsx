77bcd50c0b4e240be51f31509cc6b83da4772551
muritador5050 <muritador5050@gmail.com>
2025-09-23 22:37:15 +0100

    i added manual installation

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
+import PwaInstaller from './services/PwaInstaller';
 import './index.css';
 
 import 'virtual:pwa-register';
@@ -9,6 +10,7 @@ import 'virtual:pwa-register';
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
+      <PwaInstaller />
       <App />
     </ChakraProvider>
   </React.StrictMode>
